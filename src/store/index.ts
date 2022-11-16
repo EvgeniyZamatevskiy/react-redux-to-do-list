ab6ffc0644848fe23af7c37e06fe6096ad13d980
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-11-16 10:47:59 +0300

    Commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit"
 import appSlice from "./slices/app"
+import authSlice from "./slices/auth"
 import toDoListsSlice from "./slices/toDoLists"
 import tasksSlice from "./slices/tasks"
-import authSlice from "./slices/auth"
 
 export const store = configureStore({
   reducer: {
     app: appSlice,
+    auth: authSlice,
     toDoLists: toDoListsSlice,
     tasks: tasksSlice,
-    auth: authSlice,
-  },
+  }
 })
 
 export type RootStateType = ReturnType<typeof store.getState>
