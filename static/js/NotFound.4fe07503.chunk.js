5e140bc8ad5c77d7310e1e9639ea313c5cb54f6b
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-11-22 16:55:57 +0300

    Updates

---
+++ static/js/NotFound.4fe07503.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_to_do_list=self.webpackChunkreact_to_do_list||[]).push([[187],{8217:function(n,t,o){o.r(t),o.d(t,{NotFound:function(){return r}});o(2791);var u="notFound_notFound__r9ONQ",e=o(184),r=function(){return(0,e.jsx)("h1",{className:u,children:"404 page not found"})}}}]);
+//# sourceMappingURL=NotFound.4fe07503.chunk.js.map
